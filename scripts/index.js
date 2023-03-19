8f1e63470cde557b059587d0d298a463e2c439f3
JennySaunders <jsaunders@valoranthealth.com>
2023-03-19 05:45:57 -0400

    add a scroll effect

--- scripts/index.js
+++ scripts/index.js
@@ -4,10 +4,28 @@ const paragraphs = document.querySelector("p");
 const pageLinks = document.querySelectorAll("a");
 const navBar = document.querySelector(".nav");
 const navIcons = document.querySelectorAll(".nav__icon");
-const dayLogo = document.querySelector(".nav__day-logo");
-const nightLogo = document.querySelector(".nav__night-logo");
+const dayLogo = document.querySelector(".nav__day-logo-link");
+const nightLogo = document.querySelector(".nav__night-logo-link");
 const dayImage = document.querySelector(".switch__day-image");
 const nightImage = document.querySelector(".switch__night-image");
+
+function scrollSmoothly() {
+  let smoothScrolls = document.querySelectorAll(".smoothScroll");
+
+  for (let i = 0; i < smoothScrolls.length; i++) {
+    let windowHeight = window.innerHeight;
+    let sectionTop = smoothScrolls[i].getBoundingClientRect().top;
+    let sectionVisible = 150;
+
+    if (sectionTop < windowHeight - sectionVisible) {
+      smoothScrolls[i].classList.add("active");
+    } else {
+      smoothScrolls[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", scrollSmoothly);
 
 function toggleLight() {
   dayImage.classList.toggle("switch-image-off");
